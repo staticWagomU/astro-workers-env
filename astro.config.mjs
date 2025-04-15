ee68012f04d56956d694d766d6d2761573781c2a
staticWagomU <to6wa17@gmail.com>
2025-04-15 21:54:10 +0900

    もろもろの設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,30 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
+import { loadEnv } from 'vite';
 
 import cloudflare from '@astrojs/cloudflare';
 
+const { STATIC } = loadEnv('', process.cwd(), '');
+console.log('========== astro.config.mjs ==========');
+console.log(`STATIC: '${STATIC}'`);
+console.log(`process.env.STATIC: ${process.env.STATIC}`);
+console.log(`NO_INDEX: ${import.meta.env.NO_INDEX}`);
+console.log('======================================')
+
+const STATIC_BUILD = STATIC === 'true';
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare({
+  env: {
+    schema: {
+      STATIC: envField.boolean({ context: 'client', access: 'public' }),
+    },
+  },
+  output: STATIC_BUILD ? 'static' : 'server',
+  adapter: STATIC_BUILD ? undefined : cloudflare({
     platformProxy: {
-      enabled: true
-    }
-  })
+      enabled: true,
+      configPath: './wrangler.jsonc',
+    },
+  }),
 });
