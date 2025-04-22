64d23bed667a1ffe0bc2fad4ab544a1927c1e0eb
staticWagomU <to6wa17@gmail.com>
2025-04-22 23:28:04 +0900

    これでいいかな

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,26 @@
 // @ts-check
-import { defineConfig, envField } from 'astro/config';
+import { envField } from 'astro/config';
+import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
-const STATIC = process.env.STATIC || 'false';
+const STATIC = import.meta.env.VITE_STATIC || 'false';
+
+console.log(
+  '\n========== astro.config.js ==========\n',
+  `import.meta.env.VITE_STATIC: ${import.meta.env.VITE_STATIC}\n`,
+  `process.env.VITE_STATIC    : ${process.env.VITE_STATIC}`,
+  '\n====================================');
 
 const STATIC_BUILD = STATIC === 'true';
 
 // https://astro.build/config
 export default defineConfig({
-  // env: {
-  //   schema: {
-  //     STATIC: envField.boolean({ context: 'client', access: 'public' }),
-  //   },
-  // },
+  env: {
+    schema: {
+      VITE_STATIC: envField.boolean({ context: 'client', access: 'public' }),
+    },
+  },
   output: STATIC_BUILD ? 'static' : 'server',
   adapter: STATIC_BUILD ? undefined : cloudflare({
     platformProxy: {
