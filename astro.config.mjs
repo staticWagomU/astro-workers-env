7967c52dfc18311bc97f2068b55eed3254460a1f
staticWagomU <to6wa17@gmail.com>
2025-04-22 22:47:37 +0900

    feat: これでどうかな

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
-import { loadEnv } from 'vite';
 
 import cloudflare from '@astrojs/cloudflare';
 
-const { STATIC } = loadEnv('', process.cwd(), '');
-console.log('========== astro.config.mjs ==========');
-console.log(`STATIC: '${STATIC}'`);
-console.log(`process.env.STATIC: ${process.env.STATIC}`);
-console.log(`NO_INDEX: ${import.meta.env.NO_INDEX}`);
-console.log('======================================')
+const STATIC = process.env.STATIC || 'false';
 
 const STATIC_BUILD = STATIC === 'true';
 
