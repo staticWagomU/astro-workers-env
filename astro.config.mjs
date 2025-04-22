09024fc4d61c649682f76a05d95fa786ee184f24
staticWagomU <to6wa17@gmail.com>
2025-04-23 08:28:01 +0900

    astro:env/serverを使ってみる

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
-// import { envField } from 'astro/config';
+import { envField } from 'astro/config';
 import { defineConfig } from 'astro/config';
+/* [vite] (ssr) Error when evaluating SSR module /home/beige/dev/github.com/wagomu-no-sunaba/etude-astro-workers-env/astro.config.mjs: Cannot find module 'astro:env/server' imported from '/home/beige/dev/github.com/wagomu-no-sunaba/etude-astro-workers-env/astro.config.mjs' */
+// import  { VITE_STATIC } from 'astro:env/server';
 
 import cloudflare from '@astrojs/cloudflare';
 
@@ -8,19 +10,20 @@ const STATIC = String(import.meta.env.VITE_STATIC);
 
 console.log(
   '\n========== astro.config.js ==========\n',
-  `import.meta.env.VITE_STATIC: ${import.meta.env.VITE_STATIC}\n`,
-  `process.env.VITE_STATIC    : ${process.env.VITE_STATIC}`,
-  '\n====================================');
+  `import.meta.env.VITE_STATIC : ${import.meta.env.VITE_STATIC}\n`,
+  `process.env.VITE_STATIC     : ${process.env.VITE_STATIC}\n`,
+  // `astro:env/server VITE_STATIC: ${VITE_STATIC}\n`,
+  '======================================');
 
 const STATIC_BUILD = STATIC === 'true';
 
 // https://astro.build/config
 export default defineConfig({
-  // env: {
-  //   schema: {
-  //     VITE_STATIC: envField.boolean({ context: 'client', access: 'public' }),
-  //   },
-  // },
+  env: {
+    schema: {
+      VITE_STATIC: envField.string({ context: 'server', access: 'public' }),
+    },
+  },
   output: STATIC_BUILD ? 'static' : 'server',
   adapter: STATIC_BUILD ? undefined : cloudflare({
     platformProxy: {
